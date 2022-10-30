86b3352a68871e32aeaa69a309522b6ffd998731
be9904 <be9904@gmail.com>
2022-10-31 01:54:04 +0900

    add start screen func

--- tcp/client.py
+++ tcp/client.py
@@ -1,4 +1,35 @@
+from http import client
 from socket import *
+
+# start screen as function
+# generates login, logout, register, exit, close server messages
+def start_screen(action, socket):
+    # login
+    if action == '1':
+        user_id = input('ID : ')
+        user_pw = input('PW : ')
+        socket.send(login_msg(user_id, user_pw).encode())
+    # logout
+    elif action == '2':
+        socket.send('logout'.encode())
+    # register
+    elif action == '3':
+        user_id = input('ID : ')
+        user_pw = input('PW : ')
+        socket.send(register_msg(user_id, user_pw).encode())
+    # exit
+    elif action == '4':
+        socket.send('disconnect'.encode())
+    # close server from client (will be removed)
+    elif action == '5':
+        socket.send('close server'.encode())
+    # error, try again
+    else:
+        print('다시 선택하세요')
+        print('------------------------------')
+        return False
+    
+    return True
 
 # login msg func
 def login_msg(id, pw):
@@ -21,30 +52,10 @@ clientSocket.connect((serverIP, serverPort))
 # start loop
 while True:
     # send message to server
-    action = int(input('수행할 작업을 선택하세요:\n(1) 로그인\n(2) 로그아웃\n(3) 가입\n(4) 종료\n'))
-    # login
-    if action == 1:
-        user_id = input('ID : ')
-        user_pw = input('PW : ')
-        clientSocket.send(login_msg(user_id, user_pw).encode())
-    # logout
-    elif action == 2:
-        clientSocket.send('logout'.encode())
-    # register
-    elif action == 3:
-        user_id = input('ID : ')
-        user_pw = input('PW : ')
-        clientSocket.send(register_msg(user_id, user_pw).encode())
-    # exit
-    elif action == 4:
-        clientSocket.send('disconnect'.encode())
-    # close server from client (will be removed)
-    elif action == 5:
-        clientSocket.send('close server'.encode())
-    # error, try again
-    else:
-        print('다시 선택하세요')
-        print('------------------------------')
+    action = input('수행할 작업을 선택하세요:\n(1) 로그인\n(2) 로그아웃\n(3) 가입\n(4) 종료\n')
+    
+    # run start screen functionalities
+    if start_screen(action, clientSocket) is False:
         continue
 
     # receive reply from server
